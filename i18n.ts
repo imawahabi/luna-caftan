10757b7042cca68ec81ed41d73a1afd3595b4ce7
imawahabi <barchaliner@gmail.com>
2025-11-10 11:23:42 +0300

    edit contact us title

--- i18n.ts
+++ i18n.ts
@@ -8,7 +8,7 @@ const resources = {
         home: 'الرئيسية',
         about: 'عن لونا',
         collection: 'القفاطين',
-        contact: 'اتصلي بنا',
+        contact: 'تواصلي معنا',
       },
       home: {
         hero: {
@@ -25,7 +25,7 @@ const resources = {
         subtitle: 'تشكيلة فاخرة',
       },
       contact: {
-        title: 'اتصلي بنا',
+        title: 'تواصلي معنا',
       },
     },
   },
